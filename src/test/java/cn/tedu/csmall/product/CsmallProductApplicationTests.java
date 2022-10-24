9180d5d2bb4025a0dedb114aa9364b674cb2b778
成恒 <chengheng@tedu.cn>
2022-10-24 11:48:26 +0800

    .

---
+++ src/test/java/cn/tedu/csmall/product/CsmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
