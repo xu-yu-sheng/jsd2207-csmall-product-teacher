62d9f9c6717db64512a02f07a0c110a13c52329b
成恒 <chengheng@tedu.cn>
2022-10-26 15:17:49 +0800

    .

--- src/main/java/cn/tedu/csmall/product/service/impl/AlbumServiceImpl.java
+++ src/main/java/cn/tedu/csmall/product/service/impl/AlbumServiceImpl.java
@@ -2,8 +2,10 @@ package cn.tedu.csmall.product.service.impl;
 
 import cn.tedu.csmall.product.mapper.AlbumMapper;
 import cn.tedu.csmall.product.pojo.dto.AlbumAddNewDTO;
+import cn.tedu.csmall.product.pojo.entity.Album;
 import cn.tedu.csmall.product.service.IAlbumService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,14 +32,21 @@ public class AlbumServiceImpl implements IAlbumService {
         // 从参数对象中获取相册名称
         String albumName = albumAddNewDTO.getName();
         // 检查相册名称是否已经被占用（相册表中是否已经存在此名称的数据）
+        log.debug("检查相册名称是否已经被占用");
         int count = albumMapper.countByName(albumName);
         if (count > 0) {
             // 是：相册名称已经被占用，添加相册失败，抛出异常
             log.debug("相册名称已经被占用，添加相册失败，将抛出异常");
-        } else {
-            // 否：相册名称没有被占用，则向相册表中插入数据
-            log.debug("相册名称没有被占用，将向相册表中插入数据");
+            throw new RuntimeException();
         }
+
+        // 否：相册名称没有被占用，则向相册表中插入数据
+        log.debug("相册名称没有被占用，将向相册表中插入数据");
+        Album album = new Album();
+        BeanUtils.copyProperties(albumAddNewDTO, album);
+        log.debug("即将插入相册数据：{}", album);
+        albumMapper.insert(album);
+        log.debug("插入相册数据完成");
     }
 
 }
---
+++ src/test/java/cn/tedu/csmall/product/service/AlbumServiceTests.java
@@ -0,0 +1,31 @@
+package cn.tedu.csmall.product.service;
+
+import cn.tedu.csmall.product.pojo.dto.AlbumAddNewDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class AlbumServiceTests {
+
+    @Autowired
+    IAlbumService service;
+
+    @Test
+    void addNew() {
+        AlbumAddNewDTO albumAddNewDTO = new AlbumAddNewDTO();
+        albumAddNewDTO.setName("测试数据1");
+        albumAddNewDTO.setDescription("测试数据的简介1");
+        albumAddNewDTO.setSort(100);
+
+        try {
+            service.addNew(albumAddNewDTO);
+            log.debug("测试添加数据成功！");
+        } catch (RuntimeException e) {
+            log.debug("测试添加数据失败！");
+        }
+    }
+
+}
