a58bce47d06c5b27e6f34b0de851ca2e08d1df51
成恒 <chengheng@tedu.cn>
2022-11-06 22:54:02 +0800

    .

--- src/main/java/cn/tedu/csmall/product/controller/BrandController.java
+++ src/main/java/cn/tedu/csmall/product/controller/BrandController.java
@@ -2,12 +2,15 @@ package cn.tedu.csmall.product.controller;
 
 import cn.tedu.csmall.product.pojo.dto.BrandAddNewDTO;
 import cn.tedu.csmall.product.service.IBrandService;
+import cn.tedu.csmall.product.web.JsonResult;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,8 +41,18 @@ public class BrandController {
     public String addNew(BrandAddNewDTO brandAddNewDTO) {
         log.debug("开始处理【添加品牌】的请求：{}", brandAddNewDTO);
         brandService.addNew(brandAddNewDTO);
-        log.debug("添加相册成功！");
         return "添加相册成功！";
+    }
+
+    // http://localhost:9080/brands/9527/delete
+    @ApiOperation("删除品牌")
+    @ApiOperationSupport(order = 200)
+    @ApiImplicitParam(name = "id", value = "品牌id", required = true, dataType = "long")
+    @PostMapping("/{id:[0-9]+}/delete")
+    public JsonResult<Void> delete(@PathVariable Long id) {
+        log.debug("开始处理【删除品牌】的请求，参数：{}", id);
+        brandService.delete(id);
+        return JsonResult.ok();
     }
 
 }
